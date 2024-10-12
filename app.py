6f4c8480708c7915da8db40e8290f22251545727
josfam <jamukun@proton.me>
2024-10-12 13:44:10 +0300

    Add a button that closes the entire application

--- app.py
+++ app.py
@@ -10,4 +10,11 @@ setup_window(root)
 # place a label on the root window and make it visible (pack)
 message = ttk.Label(root, text='Hello, World!').pack()
 
+# place a button that closes the application
+def close_app():
+    """closes the application"""
+    root.destroy()
+
+close_btn = ttk.Button(root, text='Close', command=close_app).pack()
+
 root.mainloop() # keep the window visible on screen
