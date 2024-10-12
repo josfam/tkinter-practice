6ea3acf90acb7462d141fbb423f3d677b5e74e04
josfam <jamukun@proton.me>
2024-10-12 13:13:38 +0300

    Make the window non-resizable

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ root = tk.Tk() # create app window
 root.title('Tk experiments')
 icon = tk.PhotoImage(file='./assets/icons/conway-lovelace.png')
 root.iconphoto(True, icon)
+root.resizable(False, False)
 
 # place a label on the root window
 message = tk.Label(root, text='Hello, World!')
