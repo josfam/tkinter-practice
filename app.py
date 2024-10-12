a1ed5685cea1c7a5e378871c1c87f093b545483e
josfam <jamukun@proton.me>
2024-10-12 12:27:26 +0300

    Add title, and utils for centering the window

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python3
 
 import tkinter as tk
+from utils.window_utils import center_window
 
 root = tk.Tk() # create app window
+root.title('Tk experiments')
 
 # place a label on the root window
 message = tk.Label(root, text='Hello, World!')
 # make widget visible
 message.pack()
+
+center_window(root)
 
 root.mainloop() # keep the window visible on screen
---
+++ utils/window_utils.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+
+def center_window(root=None, window_width=800, window_height=800):
+    screen_width = root.winfo_screenwidth()
+    screen_height = root.winfo_screenheight()
+
+    x_center = int(screen_width / 2 - window_width / 2)
+    y_center = int(screen_height / 2 - window_height / 2)
+
+    # position the window to the center of the screen
+    root.geometry(f'{window_width}x{window_height}+{x_center}+{y_center}')
